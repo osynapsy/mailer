4a6a63b9f97038f1e9d66c75499c00fb510fd794
Pietro Celeste <p.celeste@osynapsy.net>
2025-11-14 11:06:20 +0100

    Added license disclaimer + license file

--- src/Email/Attachment.php
+++ src/Email/Attachment.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Osynapsy mailer package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Osynapsy\Mailer\Email;
 
 /**
@@ -6,35 +15,35 @@ namespace Osynapsy\Mailer\Email;
  *
  * @author Pietro Celeste <p.celeste@osynapsy.net>
  */
-class Attachment 
+class Attachment
 {
     protected $filepath;
     protected $filename;
     protected $type;
     protected $content;
-    
+
     public function __construct($path, $content = null)
     {
         $this->filepath = $path;
         $this->content = $content;
     }
-    
+
     public function getContent()
     {
         return $this->content ?? file_get_contents($this->filepath);
     }
-    
+
     public function getFilename()
     {
         return basename($this->filepath);
     }
-    
+
     public function getType()
-    {    
+    {
         $mine = new \finfo(\FILEINFO_MIME_TYPE);
-        return $mine->file($this->filepath);    
+        return $mine->file($this->filepath);
     }
-    
+
     public function __toString()
     {
         return strval(new AttachmentBuilder($this));
